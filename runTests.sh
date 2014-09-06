e48a7aa7e966a29733e607797da1a46894a4e6b8
Jan Rabe <jan.rabe@wooga.net>
2014-09-06 15:07:02 +0200

    ready-to-use

--- runTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xbuild Promises.sln /verbosity:minimal
-if [ $? = 0 ]
-then
-	mono Tests/Libraries/nspec/NSpecRunner.exe Tests/bin/Debug/Tests.dll
-else
-	echo "WARNING: Could not compile!"
-fi
